cf3929659d140511955761207b5ad4a044a65a69
Vladimir Andreev <105918724+VladimirAndreev1982@users.noreply.github.com>
2022-07-05 09:31:18 +0300

    Change_2

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
-        double body_weight = 76;// масса тела в килограммах
-        double body_growth = 174;// рост тела в сантиметрах
-        double body_mass_index = service.calculate(body_weight, body_growth);// индекс массы тела
-        System.out.println("При массе тела " + body_weight + " кг и росте " + body_growth + " см индекс массы тела " + body_mass_index);
+        double bodyWeight = 76;// масса тела в килограммах
+        double bodyGrowth = 174;// рост тела в сантиметрах
+        double bodyMassIndex = service.calculate(bodyWeight, bodyGrowth);// индекс массы тела
+        System.out.println("При массе тела " + bodyWeight + " кг и росте " + bodyGrowth + " см индекс массы тела " + bodyMassIndex);
     }
 }
